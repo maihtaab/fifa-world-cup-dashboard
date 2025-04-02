0e644c571f1cc3ba28f7c88ba6cd7171d82dac96
aiyalol <125110257+aiyalol@users.noreply.github.com>
2025-04-01 23:44:41 -0400

    Update fifa_dashboard.py

--- fifa_dashboard.py
+++ fifa_dashboard.py
@@ -71,5 +71,8 @@ def update_year(year):
         return f"In {year}, {row['Winner'].values[0]} won, and {row['Runner-Up'].values[0]} was the runner-up."
     return ""
 
+import os
+
 if __name__ == '__main__':
-  app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=True)
